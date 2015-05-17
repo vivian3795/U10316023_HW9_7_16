afee3106781693c851ad6987c85f2a8d93de6605
vivian3795 <vivian3795@gmail.com>
2015-05-17 23:14:35 +0800

    Update Sorting.java
    plus comments

--- Sorting.java
+++ Sorting.java
@@ -6,8 +6,11 @@
  */
 public class Sorting {
   public static void main(String[] args) {
+	//the question request 
     int m[][] = { { 4, 2 }, { 1, 7 }, { 4, 5 }, { 1, 2 }, { 1, 1 }, { 4, 1 } };
+	//sort m
     sort(m);
+	//print it out
     printArray(m);
   }
 
@@ -17,12 +20,15 @@ public class Sorting {
       int dot = i;
 
       for (int j = i; j < m.length; j++) {
+		// check whether need to change or not. now > m[j][0] check first number.
+		// if the first number equal to now, check the second number.
         if (now > m[j][0] || (now == m[j][0] && m[dot][1] > m[j][1])){ 
-			    now = m[j][0];
-			    dot = j;
+			now = m[j][0];
+			dot = j;
         }
       }
 
+      // if the conditions are satisfied, change them
       if (dot != i) {
         int a = m[dot][0];
         int b = m[dot][1];
@@ -35,8 +41,9 @@ public class Sorting {
   }
 	
   public static void printArray(int m[][]) {
+	//print it out
     for (int i = 0; i < m.length; i++) {
-      System.out.println(m[i][0] + ", " + m[i][1]);
+      System.out.println("{" +m[i][0] + ", " + m[i][1] + "}");
     }
   }
 
